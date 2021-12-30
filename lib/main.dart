dea6c9df8db24030063ca03b508830087c92980c
xiaoqiyang <xiaoqiyang@xique.com>
2021-12-30 16:31:21 +0800

    1

--- lib/main.dart
+++ lib/main.dart
@@ -13,8 +13,46 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        primarySwatch: Colors.indigo,
+        textTheme: const TextTheme(
+          headline1: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Color(0xff333333),
+          ),
+          headline2: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Color(0xff333333),
+          ),
+          headline3: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            color: Color(0xff333333),
+          ),
+          headline4: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+            color: Color(0xff333333),
+          ),
+          bodyText1: TextStyle(
+            fontSize: 14,
+            color: Color(0xff333333),
+          ),
+          bodyText2: TextStyle(
+            fontSize: 12,
+            color: Color(0xff333333),
+          ),
+          subtitle1: TextStyle(
+            fontSize: 12,
+            color: Color(0xff999999),
+          ),
+        ),
+      ),
+      darkTheme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      themeMode: ThemeMode.system,
       routes: {'/login': (context) => const LoginPage()},
       initialRoute: '/login',
     );
--- lib/pages/login/index.dart
+++ lib/pages/login/index.dart
@@ -10,9 +10,108 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: Text("login"),
+    return Theme(
+      data: Theme.of(context).copyWith(),
+      child: Scaffold(
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    alignment: Alignment.center,
+                    width: 48,
+                    height: 48,
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).colorScheme.primary,
+                      borderRadius: BorderRadius.circular(24),
+                    ),
+                    child: Icon(
+                      Icons.shopping_bag,
+                      color: Theme.of(context).selectedRowColor,
+                    ),
+                  ),
+                ],
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 20),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "手机验证码登录",
+                          style: Theme.of(context).textTheme.headline1,
+                        )
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "欢迎使用Daisy商城",
+                          style: Theme.of(context).textTheme.subtitle1,
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                margin: const EdgeInsets.only(top: 40),
+                height: 44,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Expanded(
+                      child: TextField(
+                        keyboardType: TextInputType.phone,
+                        textAlignVertical: TextAlignVertical.center,
+                        decoration: InputDecoration(
+                          hintText: "请输入手机号",
+                          prefixIcon: Icon(Icons.phone),
+                          isCollapsed: true,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(50)),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                margin: const EdgeInsets.only(top: 20),
+                height: 44,
+                alignment: Alignment.center,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Expanded(
+                      child: TextField(
+                        textAlignVertical: TextAlignVertical.center,
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(
+                          hintText: "请输入验证码",
+                          prefixIcon: Icon(Icons.chat_bubble_outline),
+                          isCollapsed: true,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(50)),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
